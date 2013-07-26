b2e525fd3ea0b5d340be988d670d99f8b6d16b3a
kirkjwalker <kirkjwalker@yahoo.com>
2013-07-26 00:36:14 +0000

    Edited product desc in index.html

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
-//#!/usr/bin/env node
+#!/usr/bin/env node
 
 var fs = require('fs');
 var express = require('express');
@@ -13,7 +13,7 @@ app.get('/', function(request, response) {
   response.send(buffer.toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
